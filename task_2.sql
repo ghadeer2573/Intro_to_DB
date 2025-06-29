1eb2c8b04bb7144248bdc19bf34ce3edc98f512c
Ghadeer <ghadeer.raafat@gmail.com>
2025-06-29 22:08:01 +0300

    create magical tables

--- task_2.sql
+++ task_2.sql
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS Orders (
 );
 
 -- Create order_details table
-CREATE TABLE IF NOT EXISTS Order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
